346f5b5209986941b2a7daa7bcf52484eb79a930
Johnathan Beal <johnathanbeal@gmail.com>
2020-01-17 06:27:27 -0500

    created Parabool class

---
+++ Battleship/Parabool.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Battleship
+{
+    public class Parabool
+    {
+        public Parabool(bool hasAShip, bool hasAHit)
+        {
+            HasAShip = hasAShip;
+            HasAHit = hasAHit;
+        }
+
+        public bool HasAShip { get; private set; }
+
+        public bool HasAHit { get; private set; }
+    }
+}
